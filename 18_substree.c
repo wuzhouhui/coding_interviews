11309b812798c75bc12e27b26da97ea3dd4ac44e
wuzhouhui <wuzhouhui250@gmail.com>
2016-06-07 21:26:30 +0800

    面试题 18: 树的子结构, 第 117 页

---
+++ 18_substree.c
@@ -0,0 +1,35 @@
+struct treenode {
+	int	val;
+	struct treenode *left;
+	struct treenode *right;
+};
+
+static int do_has(const struct treenode *root1, const struct treenode *root2)
+{
+	if (!root2)
+		return(1);
+	if (!root1)
+		return(0);
+	if (root1->val != root2->val)
+		return(0);
+	return(do_has(root1->left, root2->left) && do_has(root1->right,
+				root2->right));
+}
+
+int hassubtree(const struct treenode *root1, const struct treenode *root2)
+{
+	if (!root1 || !root2)
+		return(0);
+	int	r = 0;
+	if (root1->val == root2->val)
+		r = do_has(root1, root2);
+	if (!r)
+		r = do_has(root1->left, root2);
+	if (!r)
+		r = do_has(root1->right, root2);
+	return(r);
+}
+
+int main(void)
+{
+}
